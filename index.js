126570ebe92daf9b6e0b617cbc78e334aac5b93c
shivam123425 <shivam123425@gmail.com>
2018-12-29 00:11:34 +0530

    train and analyse the m and b values

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("@tensorflow/tfjs-node");
 const tf = require("@tensorflow/tfjs");
 
 const loadCSV = require("./load-csv");
+const LinearRegression = require("./linear-regression");
 
 let { features, labels, testFeatures, testLabels } = loadCSV("./cars.csv", {
   shuffle: true,
@@ -10,4 +11,12 @@ let { features, labels, testFeatures, testLabels } = loadCSV("./cars.csv", {
   labelColumns: ["mpg"]
 });
 
-console.log(features, labels);
+const regression = new LinearRegression(features, labels, {
+  learningRate: 0.001,
+  iterations: 100
+});
+
+regression.train();
+
+console.log("Updated value of M:", regression.m);
+console.log("Updated value of B:", regression.b);
